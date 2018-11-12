75344f1fad5316918caf5c3e6cc6f77ac652a08d
William Henney <will@henney.org>
2018-11-11 18:40:13 -0600

    Update copyright notice, changes of phrasing

--- image2fits.py
+++ image2fits.py
@@ -3,8 +3,9 @@ image2fits.py - Convert an RGB image to 3 FITS channels
 
 Author: William Henney 
 Version 0.1 - 07 Apr 2011
-Version 0.2 - 24 Feb 2017 - Updated for python3 and astropy
+Version 0.2 - 24 Feb 2017 - Update for python3 and astropy
 Version 0.3 - 23 Nov 2017 - Add support for RGBA (but we ignore the alpha channel)
+Version 0.4 - 08 Aug 2018 - Track astropy API changes
 """
 from __future__ import print_function
 import numpy
@@ -18,18 +19,17 @@ from PIL import Image
 import argparse
 
 def add_comments(hdu):
-    hdu.header.add_comment("Written by image2fits.py, Will Henney 2011,2012,2017")
+    hdu.header.add_comment("Written by image2fits.py, Will Henney 2011-2018")
     hdu.header.add_comment("Converted from  %s" % (args.file.name))
     
 
 parser = argparse.ArgumentParser(description="Convert an RGB or Grayscale image to FITS format")
 parser.add_argument("file", type=argparse.FileType('rb'),  help='Image file to convert')
 parser.add_argument("--wcs", action="store_true", 
-                    help='Add WCS info to FITS file header')
+                    help='Add WCS info to FITS file header (NOT IMPLEMENTED YET)')
 
 args = parser.parse_args()
 
-# Python 3 requires that file be opened in binary mode
 im = Image.open(args.file)           # read the image
 assert im.mode in ["RGBA", "RGB", "L" ], \
     "File %s is of type '%s', which is not supported" % (args.file, im.mode)
